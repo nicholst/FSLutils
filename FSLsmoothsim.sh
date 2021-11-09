854ea1d296504041492dd63553b91bec63a7c126
nicholst <thomas.nichols@bdi.ox.ac.uk>
2021-11-09 17:02:13 +0000

    Add FSLsmoothsim.sh

---
+++ FSLsmoothsim.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+Nimg=20 # Number of images to simulate
+FWHM=9  # FWHM in mm
+
+sigma=$(echo "$FWHM / 2.355" | bc -l)
+FD=$FSLDIR/data/standard
+
+# Calibrate SD
+fslmaths $FD/MNI152_T1_2mm -mul 0 -randn -kernel gauss 5 -fmean -mas $FD/MNI152_T1_2mm_brain_mask /tmp/$$
+sd=$(fslstats /tmp/$$ -S)
+fslmaths /tmp/$$ -mul 0 /tmp/$$
+gunzip /tmp/$$.nii.gz # Uncompress once, instead of repeatedly inside the loop
+
+# Simulate!
+for i in $(seq $Nimg) ; do
+    fNm=$(printf "SimImg_%05d" $i)
+    fslmaths /tmp/$$ -seed $RANDOM \
+	     -randn -kernel gauss $sigma -fmean \
+	     -div $sd \
+	     -mas $FD/MNI152_T1_2mm_brain_mask \
+	     $fNm
+done
+imrm /tmp/$$
+
